7f865fcb9baf0282553c668b9c531bef29c62517
liuming <1969489778@qq.com>
2016-05-03 19:05:19 +0800

    liuming
    创建项目

---
+++ ocBase/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ocBase
+//
+//  Created by 明刘 on 16/5/3.
+//  Copyright © 2016年 liuming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
